e5624c37e566a4d190db1abe1e97cd8efa2971db
LiangLuDev <LiangLuDev@gmail.com>
2018-05-15 17:44:58 +0800

    Rx操作符-组合/合并操作符

---
+++ app/src/main/java/com/luliang/rxpractice/operators/combine/ObservablesCombine.java
@@ -0,0 +1,15 @@
+package com.luliang.rxpractice.operators.combine;
+
+/**
+ * Created by LuLiang on 2018/5/15.
+ * 组合多个被观察者
+ * 按数量：zip
+ * 按时间：combineLatest、combineLatestDelayError
+ * 组合一个事件发送：reduce、collect
+ *
+ * @author LuLiang
+ * @github https://github.com/LiangLuDev
+ */
+public class ObservablesCombine {
+
+}
---
+++ app/src/main/java/com/luliang/rxpractice/operators/combine/ObservablesMerge.java
@@ -0,0 +1,15 @@
+package com.luliang.rxpractice.operators.combine;
+
+/**
+ * Created by LuLiang on 2018/5/15.
+ * 合并多个被观察者
+ * 按发送顺序：concat、concatArray
+ * 按时间：merge、mergeArray
+ * 按错误处理：concatDelayError、mergeDelayError
+ *
+ * @author LuLiang
+ * @github https://github.com/LiangLuDev
+ */
+public class ObservablesMerge {
+    
+}
